9e344c01f4c5a15655ed2b6835e6646e5cf4f5df
Jagannati Harsha Vardhan <jharshavardhan28@gmail.com>
2026-04-01 20:42:28 +0530

    Add print statements for data cleaning steps
    Added print statements to show the number of entries after removing duplicates and null values.

--- task2_data_processing.py
+++ task2_data_processing.py
@@ -22,9 +22,11 @@ print(f"Loaded {len(df)} posts from {latest_file}")
 
 # remove duplicates based on post id
 df.drop_duplicates(subset='post_id' , inplace=True)
+print(f"After removing duplicates: {len(df)}")
 
 # Drop rows where essential fields are missing values 
 df.dropna(subset=['post_id','title','score'] , inplace=True)
+print(f"After removing nulls: {len(df)}")
 
 #Data types — make sure score and num_comments are integers
 df['score']= df['score'].astype(int)
